1482b16dcc0a5796b83e858075882f937b41f3de
DSanzh <dauylov.sanzhar@gmail.com>
2017-07-22 11:08:50 +0600

    finish till 21s video tutorial

--- FirstProject/music/views.py
+++ FirstProject/music/views.py
@@ -1,16 +1,10 @@
-from django.http import HttpResponse
 from .models import Album
-from django.template import loader
-from django.urls import resolvers
+from django.shortcuts import render, get_object_or_404
 
 def index(request):
     all_albums = Album.objects.all()
-    template = loader.get_template('music/index.html')
-    context = {
-        'all_albums': all_albums,
-    }
-    return HttpResponse(template.render(context, request))
-
+    return render(request, 'music/index.html', {'all_albums' : all_albums})
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for Album id: " + str(album_id) + '</h2>')
+    album = get_object_or_404(Album, pk=album_id)
+    return render(request, 'music/detail.html', { 'album' : album})
