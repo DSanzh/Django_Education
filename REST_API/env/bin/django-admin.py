edc4747ec0934cf98330aef0b11ac541874a85e4
Sanzhar <dauylov.sanzhar@gmail.com>
2017-11-15 16:13:40 +0600

    REST_API: Stock List

---
+++ REST_API/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/admin/Desktop/Projects/EDU/Django_Education/REST_API/env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
